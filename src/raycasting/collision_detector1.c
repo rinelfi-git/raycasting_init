d1d2ecf4fffe1c22e78321ff97ddccf34d7199e1
erijania <erijania@student.42antananarivo.mg>
2025-05-28 15:03:44 +0300

    impouve collision detector

--- src/raycasting/collision_detector1.c
+++ src/raycasting/collision_detector1.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 23:16:42 by erijania          #+#    #+#             */
-/*   Updated: 2025/05/27 20:07:41 by erijania         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:02:27 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	check_hurt_up(t_will_hurt_var *var, t_player *player)
 {
-	var->map_y = player->y - (int)roundf(var->dy * (MOVE_STEP + 5));
-	var->map_x = player->x - (int)roundf(var->dx * (MOVE_STEP + 5));
+	var->map_y = player->y - (int)roundf(var->dy * (MOVE_STEP * 2));
+	var->map_x = player->x - (int)roundf(var->dx * (MOVE_STEP * 2));
 }
 
 void	check_hurt_down(t_will_hurt_var *var, t_player *player)
 {
-	var->map_y = player->y + (int)roundf(var->dy * (MOVE_STEP + 5));
-	var->map_x = player->x + (int)roundf(var->dx * (MOVE_STEP + 5));
+	var->map_y = player->y + (int)roundf(var->dy * (MOVE_STEP * 2));
+	var->map_x = player->x + (int)roundf(var->dx * (MOVE_STEP * 2));
 }
 
 void	check_hurt_left(t_will_hurt_var *var, t_player *player)
 {
-	var->map_y = player->y + (int)roundf(var->dx * (MOVE_STEP + 5));
-	var->map_x = player->x - (int)roundf(var->dy * (MOVE_STEP + 5));
+	var->map_y = player->y + (int)roundf(var->dx * (MOVE_STEP * 2));
+	var->map_x = player->x - (int)roundf(var->dy * (MOVE_STEP * 2));
 }
 
 void	check_hurt_right(t_will_hurt_var *var, t_player *player)
 {
-	var->map_y = player->y - (int)roundf(var->dx * (MOVE_STEP + 5));
-	var->map_x = player->x + (int)roundf(var->dy * (MOVE_STEP + 5));
+	var->map_y = player->y - (int)roundf(var->dx * (MOVE_STEP * 2));
+	var->map_x = player->x + (int)roundf(var->dy * (MOVE_STEP * 2));
 }
